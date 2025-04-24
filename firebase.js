081d2eab86d4ffd74ed2c4b51f3f675d4ed8eceb
Muhammad Iqbal Musyaffa Amna <148077854+i1qb4lbyt33xp1o1t@users.noreply.github.com>
2025-04-25 05:13:59 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBH-XnL0hXTF9N0HpBYwYMjvCl3Y8oVFmo",
-  authDomain: "web-stats-14444.firebaseapp.com",
-  projectId: "web-stats-14444",
-  storageBucket: "web-stats-14444.firebasestorage.app",
-  messagingSenderId: "845455411430",
-  appId: "1:845455411430:web:3c806c813858d1da399327"
+  apiKey: "AIzaSyDYo7QMiDKulRgp-4cr9rVu5F0cvcI4P34",
+  authDomain: "iqbal-8d5b6.firebaseapp.com",
+  projectId: "iqbal-8d5b6",
+  storageBucket: "iqbal-8d5b6.firebasestorage.app",
+  messagingSenderId: "930704607161",
+  appId: "1:930704607161:web:3b6f32b3441525c47b9868"
 };
 
 console.log('Initializing Firebase with config:', firebaseConfig);
